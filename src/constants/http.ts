2e59a817b7c0f8de32fa29bc3e922ee43736fcc5
Vudang <danglegiavu@gmail.com>
2021-07-04 16:42:30 +0700

    add contants

---
+++ src/constants/http.ts
@@ -0,0 +1,18 @@
+export const HTTP_RESPONSE_CODE = {
+    SUCCESS: 200,
+    NOT_FOUND : 404,
+    FAILED: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN : 403,
+    BAD_GATEWAY : 502,
+  }
+  
+  export const HTTP_RESPONSE_MESSAGE = {
+    SUCCESS:'Your request has been successfully submitted',
+    FAILED:'Your request has been rejected'
+}
+
+export const HTTP_RESPONSE_STATUS = {
+    SUCCESS:'OK',
+    FAILED:'FAILED'
+}
