ecde76fe8ab6bb86a7e2253d4437afc79a01f400
Vudang <danglegiavu@gmail.com>
2021-07-04 18:33:12 +0700

    update api layer

--- src/apis/product.api.ts
+++ src/apis/product.api.ts
@@ -1,33 +1,55 @@
+import { HTTP_RESPONSE_CODE, HTTP_RESPONSE_MESSAGE, HTTP_RESPONSE_STATUS } from "src/constants/http"
 
 const mockData: IProduct[] = [
-    {
-        id: 'test',
-        title: 'test',
-        description: 'test',
-        imgUrl: 'test',
-        thumbUrl: 'test',
-        quantity: 1,
-        price: 1,
-        provider: 'test',
-        publisher: 'test',
-        stock: 1,
-        category: 'test',
-        author: 'test'
-    }
+  {
+    id: "test",
+    title: "test",
+    description: "test",
+    imgUrl: "test",
+    thumbUrl: "test",
+    quantity: 1,
+    price: 1,
+    provider: "test",
+    publisher: "test",
+    stock: 1,
+    category: "test",
+    author: "test"
+  }
 ]
 
 export const getProductList = (): Promise<IResponseGetProductApi> => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve({
-                data: {
-                    products: mockData,
-                },
-                message:'Ok',
-                code:200,
-                status:'Ok'
-            })
-        }, 1000)
-        if(!resolve) reject()
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        data: {
+          products: mockData
+        },
+        message: "Ok",
+        code: 200,
+        status: "Ok"
+      })
+    }, 1000)
+    if (!resolve) reject()
+  })
+}
+
+
+export const getProduct = (id : string) : Promise<IResponseGetProductItemApi> => {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      const product = mockData.find(p => p.id === id)
+      if(product){
+        resolve({
+          data:{
+            product
+          },
+          message:HTTP_RESPONSE_MESSAGE.SUCCESS,
+          code:HTTP_RESPONSE_CODE.SUCCESS,
+          status:HTTP_RESPONSE_STATUS.SUCCESS
+        })
+      } else {
+        reject(new Error(HTTP_RESPONSE_MESSAGE.FAILED))
+      }
     })
+  })
 }
--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -1,28 +1,35 @@
-import { HTTP_RESPONSE_CODE, HTTP_RESPONSE_MESSAGE, HTTP_RESPONSE_STATUS } from "@/constants/http"
+import {
+  HTTP_RESPONSE_CODE,
+  HTTP_RESPONSE_MESSAGE,
+  HTTP_RESPONSE_STATUS
+} from "../constants/http"
 
-const MOCK_USER : IRequestLogin= {
-    email : "vudang@gmail.com",
-    password:"Giavuhehehe",
-    
+const MOCK_USER: IRequestLogin = {
+  email: "vudang@gmail.com",
+  password: "Giavuhehehe"
 }
 
-const MOCK_TOKEN ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MDdlYzVmNmUzYTVkMDA5MWZmNzgwMjUiLCJpYXQiOjE2MTg5OTkwMjUsImV4cCI6MTYxOTYwMzgyNX0.uMerTNgPq3CtMoWc3oQb3Oz4tXDCa-mAUBvCB2IADWA'
+const MOCK_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MDdlYzVmNmUzYTVkMDA5MWZmNzgwMjUiLCJpYXQiOjE2MTg5OTkwMjUsImV4cCI6MTYxOTYwMzgyNX0.uMerTNgPq3CtMoWc3oQb3Oz4tXDCa-mAUBvCB2IADWA"
 
-export const userLoginApi = ({email,password} : IRequestLogin) : Promise<IResponseLoginApi> => {
-    return new Promise((resolve,reject) => {
-        setTimeout(() => {
-            if(email === MOCK_USER.email && password === MOCK_USER.password){
-                resolve({
-                    data:{
-                        access_token:MOCK_TOKEN                       
-                    },
-                    status:HTTP_RESPONSE_STATUS.SUCCESS,
-                    message:HTTP_RESPONSE_MESSAGE.SUCCESS,
-                    code:HTTP_RESPONSE_CODE.SUCCESS
-                })
-            } else {
-                reject(new Error(HTTP_RESPONSE_MESSAGE.FAILED))
-            }
+export const userLoginApi = ({
+  email,
+  password
+}: IRequestLogin): Promise<IResponseLoginApi> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (email === MOCK_USER.email && password === MOCK_USER.password) {
+        resolve({
+          data: {
+            access_token: MOCK_TOKEN
+          },
+          status: HTTP_RESPONSE_STATUS.SUCCESS,
+          message: HTTP_RESPONSE_MESSAGE.SUCCESS,
+          code: HTTP_RESPONSE_CODE.SUCCESS
         })
+      } else {
+        reject(new Error(HTTP_RESPONSE_MESSAGE.FAILED))
+      }
     })
+  })
 }
