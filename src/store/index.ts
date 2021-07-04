93cfdbec2fa2f3dde4b2bb81feec5bbcda56e497
Vudang <danglegiavu@gmail.com>
2021-07-04 18:35:22 +0700

    setup redux

---
+++ src/store/index.ts
@@ -0,0 +1,22 @@
+import { Action, applyMiddleware, createStore, compose } from "redux"
+
+import reducers from "../reducers"
+import thunk, { ThunkDispatch } from "redux-thunk"
+
+const composeEnhancers =
+  typeof window === "object" &&
+  process.env.NODE_ENV === "development" &&
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose
+const enhancer = composeEnhancers(applyMiddleware(thunk))
+
+export const store = createStore(reducers, enhancer)
+
+//export const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+
+export type RootStateType = ReturnType<typeof store.getState>
+
+export type AppDispatchType = typeof store.dispatch
+
+export type ThunkDispatchType = ThunkDispatch<RootStateType, any, Action>
