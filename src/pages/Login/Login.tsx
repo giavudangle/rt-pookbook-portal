5e774156ea13beefa1afa27aed51c83886c8da5a
Vudang <danglegiavu@gmail.com>
2021-07-07 13:50:22 +0700

    update login feature

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,42 +1,69 @@
-import React, { useState } from "react"
-import { connect, ConnectedProps } from "react-redux"
-import { login } from "./Login.thunks"
-import { Title } from "./Login.styles"
-import { useHistory } from "react-router-dom"
-import { PATH } from "../../constants/paths"
 
-const mapStateToProps = state => ({
-  loading: state.loading
-})
 
-const mapDispatchToProps = {
-  login
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import {useStyles} from './Login.styles'
+import { useState } from 'react';
+import { useHistory } from 'react-router';
+import { useAppSelector } from '../../hooks/useAppSelector';
+import { useThunkDispatch } from '../../hooks/useThunkDispatch';
+import { login } from './Login.thunks';
+import { PATH } from '../../constants/paths';
+import { FormHelperText } from '@material-ui/core';
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright PookBook© '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Vudang Group
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-const connector = connect(mapStateToProps, mapDispatchToProps)
 
-interface Props extends ConnectedProps<typeof connector> {}
 
-const Login = (props: Props) => {
-  const { login, loading } = props
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState("")
-  const history = useHistory()
 
-  const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+const Login: React.FC  = () : JSX.Element => {
+  const [email,setEmail] = useState<string>("");
+  const [password,setPassword] = useState<string>("")
+  const [error,setError] = useState<string>("")
+
+  const history = useHistory();
+  const classes = useStyles();
+  const {loading} = useAppSelector(state => state.login)
+  const dispatch = useThunkDispatch();
+
+  const _handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value)
   }
 
-  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const _handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value)
   }
 
-  const submit = async (event: React.FormEvent<HTMLFormElement>) => {
+
+  
+  const _handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     if (!loading) {
       const payload = { email, password }
-      login(payload)
+      dispatch(login(payload))
         .then(res => {
           history.push(PATH.HOME)
         })
@@ -46,36 +73,79 @@ const Login = (props: Props) => {
     }
   }
 
+
+
   return (
-    <div className="container">
-      <div className="min-vh-100 row">
-        <div className="col-md-6 m-auto">
-          <form className="p-5 rounded-sm shadow text-center" onSubmit={submit}>
-            <Title>Login</Title>
-            <p className="text-muted">Please enter your login and password!</p>
-            <input
-              type="text"
-              placeholder="Email"
-              onChange={handleEmail}
-              className="form-control form-control-lg mb-4"
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <form onSubmit={_handleSubmit} className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={_handleEmail}
             />
-            <input
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
               type="password"
-              placeholder="Password"
-              onChange={handlePassword}
-              className="form-control form-control-lg mb-4"
+              id="password"
+              autoComplete="current-password"
+              onChange={_handlePassword}
             />
-            {error && (
-              <div className="mb-3 text-danger text-xl-center">{error}</div>
-            )}
-            <button type="submit" className="btn btn-block btn-info btn-lg">
-              Login
-            </button>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <FormHelperText error>{error}</FormHelperText>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
           </form>
         </div>
-      </div>
-    </div>
-  )
+      </Grid>
+    </Grid>
+  );
 }
 
-export default connector(Login)
+export default Login
