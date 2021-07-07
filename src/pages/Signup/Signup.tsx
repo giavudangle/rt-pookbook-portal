6434f8562d4b006f134d84cd201a35f5d3add314
Vudang <danglegiavu@gmail.com>
2021-07-07 13:51:07 +0700

    add signup feature

---
+++ src/pages/Signup/Signup.tsx
@@ -0,0 +1,151 @@
+
+
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import {useStyles} from './Signup.styles'
+import { useState } from 'react';
+import { useHistory } from 'react-router';
+import { useAppSelector } from '../../hooks/useAppSelector';
+import { useThunkDispatch } from '../../hooks/useThunkDispatch';
+import { login } from './Signup.thunks';
+import { PATH } from '../../constants/paths';
+import { FormHelperText } from '@material-ui/core';
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright PookBook© '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Vudang Group
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+
+
+
+const Login: React.FC  = () : JSX.Element => {
+  const [email,setEmail] = useState<string>("");
+  const [password,setPassword] = useState<string>("")
+  const [error,setError] = useState<string>("")
+
+  const history = useHistory();
+  const classes = useStyles();
+  const {loading} = useAppSelector(state => state.login)
+  const dispatch = useThunkDispatch();
+
+  const _handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value)
+  }
+
+  const _handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value)
+  }
+
+
+  
+  const _handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if (!loading) {
+      const payload = { email, password }
+      dispatch(login(payload))
+        .then(res => {
+          history.push(PATH.HOME)
+        })
+        .catch(err => {
+          setError(err.payload.message)
+        })
+    }
+  }
+
+
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <form onSubmit={_handleSubmit} className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={_handleEmail}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={_handlePassword}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <FormHelperText error>{error}</FormHelperText>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Login
