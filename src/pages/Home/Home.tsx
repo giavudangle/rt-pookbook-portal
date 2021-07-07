2b74d175263fed6d5a3c9c3c83954b6667479a6d
Vudang <danglegiavu@gmail.com>
2021-07-07 14:41:04 +0700

    integrate dashboard with rechart

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,8 @@
 import React from "react"
 import MainLayout from "../../layouts/_Shared"
+import DashBoard from "../../layouts/_Dashboard"
 export default function Home() {
   return (
-    <MainLayout>
-      <h2 className="mb-4">Home</h2>
-    </MainLayout>
+      <DashBoard/> 
   )
 }
