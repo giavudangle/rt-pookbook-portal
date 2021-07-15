97f45938091b581d5c5062bf49aa0565b4f77a50
Vudang <danglegiavu@gmail.com>
2021-07-15 19:46:44 +0700

    update reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,15 +3,20 @@ import { AppReducer } from "../app/app.reducer"
 import { loginReducer } from "../pages/Login/Login.reducer"
 import { ProductListReducer } from "../pages/Product/ProductList/ProductList.reducer"
 import { ProductItemReducer } from "../pages/Product/ProductItem/ProductItem.reducer"
-
 import { CategoryReducer } from "../pages/Category/Category.reducer"
+import { AuthorReducer } from "../pages/Author/Author.reducer"
+import { ProviderReducer } from "../pages/Provider/Provider.reducer"
+import { PublisherReducer } from "../pages/Publisher/Publisher.reducer"
 
 const rootReducer = combineReducers({
   appReducer: AppReducer,
   loginReducer: loginReducer,
   productListReducer: ProductListReducer,
   productItemReducer: ProductItemReducer,
-  categoryReducer: CategoryReducer
+  categoryReducer: CategoryReducer,
+  authorReducer: AuthorReducer,
+  providerReducer: ProviderReducer,
+  publisherReducer: PublisherReducer
 })
 
 export default rootReducer
